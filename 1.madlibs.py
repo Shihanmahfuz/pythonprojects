b70e63ac6f58d9f2e95bae24ce32517c7b42e428
Shihanmahfuz <info.shihanmahfuz@gmail.com>
2023-02-28 03:45:01 +0600

    Initial Commit

---
+++ 1.madlibs.py
@@ -0,0 +1,9 @@
+adjective = input("Enter an adjective:\n")
+compliment = input("Enter a compliment:\n")
+place = input("Enter a place:\n")
+color = input("Enter a color:\n")
+
+print("I " + adjective + " my girlfriend,")
+print("She is " + compliment + ",")
+print("I first met her at " + place + " in Bangladesh,")
+print("and I wish to give her a " + color + " rose everyday.")
